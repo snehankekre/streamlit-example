a2967cb8da6c93d848c9af728966c8424a097107
Snehan Kekre <snehan.minerva@gmail.com>
2021-06-29 11:50:53 +0530

    Update streamlit_app.py

--- packages-bug/streamlit_app.py
+++ packages-bug/streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit as st
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem import MACCSkeys, Draw
 
-try:
-    from rdkit import Chem
-    from rdkit.Chem import Draw
-    from rdkit.Chem import MACCSkeys, Draw
-    st.write('Success!')
-except:
-    st.write('Failed to install dependencies from packages.txt :(')
+# try:
+#     from rdkit import Chem
+#     from rdkit.Chem import Draw
+#     from rdkit.Chem import MACCSkeys, Draw
+#     st.write('Success!')
+# except:
+#     st.write('Failed to install dependencies from packages.txt :(')
